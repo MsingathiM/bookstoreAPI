c835baec5ed574cac4346e1f2612c21ca8661108
Msi <msi@methoddata.com>
2023-09-14 18:02:49 +0200

    adding api tests

---
+++ src/test/java/stepDefinitions/End2End_Test.java
@@ -0,0 +1,31 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class End2End_Test {
+    @Given("I am an authorised user")
+    public void iAmAnAuthorisedUser() {
+    }
+
+    @Given("A list of books are available")
+    public void aListOfBooksAreAvailable() {
+    }
+
+    @When("I add a book to my reading list")
+    public void iAddABookToMyReadingList() {
+    }
+
+    @Then("The book is added")
+    public void theBookIsAdded() {
+    }
+
+    @When("I remove a book from my reading list")
+    public void iRemoveABookFromMyReadingList() {
+    }
+
+    @Then("The book is removed")
+    public void theBookIsRemoved() {
+    }
+}
